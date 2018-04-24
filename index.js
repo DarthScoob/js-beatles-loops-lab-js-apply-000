651ae675ecb0b5a8869ace201b6e35e45732a17f
spharris5200@gmail.com <spharris5200@gmail.com>
2018-04-24 04:35:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,47 @@
+var musicians = [
+  "John Lennon", 
+  "Paul McCartney", 
+  "George Harrison", 
+  "Ringo Starr"];
+var instruments = [
+  "Guitar",
+  "Bass Guitar",
+  "Lead Guitar",
+  "Drums"];
+
+function theBeatlesPlay(musicians, instuments) {
+  var band = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    band.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return band;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  
+function johnLennonFacts(facts) {
+  let john = [];
+  let i = 0;
+  while (i < facts.length) {
+    john[i] = facts[i] + "!!!";
+    i++;
+  }
+  return john; 
+} 
+
+function iLoveTheBeatles(n) {
+  var x = [];
+  do {
+    let x = 0;
+    //x++;
+    } while (x < 15); {
+      
+     return(x);
+  }
+}
+  
